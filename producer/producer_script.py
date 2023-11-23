9f8c61c69251200a87e518c8b7e0df002dd586d2
yassinessadi <yasine.esadi@gmail.com>
2023-11-23 16:46:44 +0100

    create kafka producer to broad cast the data

--- producer/producer_script.py
+++ producer/producer_script.py
@@ -1,6 +1,22 @@
-from pykafka import KafkaClient
+from confluent_kafka import Producer
+import requests
+import json
+import time
 
+topic = "yassinecounter"
+kafka_config = {
+    "bootstrap.servers": "localhost:9092", 
+}
 
-client = KafkaClient(hosts="localhost:9092")
+producer = Producer(kafka_config)
 
-print(client)
+while True:
+    response = requests.get("http://127.0.0.1:5000/get_movies/1")
+    time.sleep(2)
+    if response.status_code == 200:
+        data = json.dumps(response.json())
+        producer.produce(topic, key="randomovie", value=data)
+        producer.flush()
+        print(response.json())
+    else:
+        print(response.status_code)
