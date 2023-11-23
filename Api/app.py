233e32ce6481e7c42186910d7c9afd6258731ed0
NADIR Hicham <hnadir.dev@gmail.com>
2023-11-23 10:37:24 +0100

    Script Api - specify number of results

--- Api/app.py
+++ Api/app.py
@@ -1,15 +1,19 @@
-from flask import Flask,jsonify
+from flask import Flask
 import json
 import pandas as pd
-
-df = pd.read_json(orient='records',path_or_buf='../data/output.json')
 
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    res = df.head(10).to_json(orient='records')
-    result = json.loads(res)
-    return jsonify(result)
+@app.route('/get_movies/<int:count>', methods=['GET'])
+
+def index(count):
+    df = pd.read_json(path_or_buf='../data/output.json',orient='records')
+
+    res = df.sample(n=count).to_json(orient='records')
+
+    return json.loads(res)
+
+if __name__ == '__main__':
+    app.run(debug=True)
