53029d24bf761cf0681c8f3e6786b962cb903b11
NADIR Hicham <hnadir.dev@gmail.com>
2023-11-24 17:02:05 +0100

    insert into the memgraph

--- consumer/consumer_script.py
+++ consumer/consumer_script.py
@@ -1,24 +1,65 @@
 from neo4j import GraphDatabase
-from confluent_kafka import Consumer
-
+from confluent_kafka import Consumer,  KafkaError
+import json
 
 URI = "bolt://localhost:7687"
 AUTH = ("", "")
 
 topic = "movietopic"
+kafka_group_id = 'group-id'
 kafka_config = {
+    'group.id': kafka_group_id,
     "bootstrap.servers": "localhost:9092", 
 }
 
-Consumer = Consumer(kafka_config)
+consumer = Consumer(kafka_config)
+consumer.subscribe([topic])
 
-# for message in consumer:
-#     if message is not None:
-#         print(message.offset, message.value)
+while True:
+    msg = consumer.poll(1.0)
+
+    if msg is None:
+        continue
+
+    if msg.error():
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            print(f"Reached end of partition {msg.topic()} [{msg.partition()}]")
+        else:
+            print(f"Error: {msg.error()}")
+    else:
+        print(f"Received message: {msg.value()}")
+        user = {}
+        movie = {}
+        genre = {}
 
 
+        data = json.loads(msg.value())
 
-# print(kClient.topics)
+        #creation of nodes
+        user['userId'] = data['userId']
+        user['rating'] = data['rating']
+        user['timestamp'] = data['timestamp']
+
+        movie['movieId'] = data['movie']['movieId']
+        movie['title'] = data['movie']['title']
+
+        with GraphDatabase.driver(URI, auth=AUTH) as client:
+    
+            # Check the connection
+            client.verify_connectivity()
+            
+            #creation movie node
+            records, summary, keys = client.execute_query("MERGE (m:Movie {movieId: $movieId,title: $title}) RETURN m.movieId AS movieId;",
+            movieId=movie['movieId'],
+            title=movie['title'],
+            database_="memgraph",
+            )
+
+            for record in records:
+                print(record["movieId"])
+        #creation relationship
+
+
 
 # with GraphDatabase.driver(URI, auth=AUTH) as client:
     
