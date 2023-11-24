b806e2d97d80d179597a6b618064c70358cd5e6b
NADIR Hicham <hnadir.dev@gmail.com>
2023-11-24 08:54:12 +0100

    Consumer script updated

--- consumer/consumer_script.py
+++ consumer/consumer_script.py
@@ -5,24 +5,35 @@ from pykafka import KafkaClient
 URI = "bolt://localhost:7687"
 AUTH = ("", "")
 
+kClient = KafkaClient(hosts="127.0.0.1:9092")
+
+topic = kClient.topics['movies_info']
+
+consumer = topic.get_simple_consumer()
+
+for message in consumer:
+    if message is not None:
+        print(message.offset, message.value)
 
 
-with GraphDatabase.driver(URI, auth=AUTH) as client:
+# print(kClient.topics)
+
+# with GraphDatabase.driver(URI, auth=AUTH) as client:
     
-    # Check the connection
-    client.verify_connectivity()
+#     # Check the connection
+#     client.verify_connectivity()
 
-    records, summary, keys = client.execute_query(
-        "MERGE (m:Movie {userId: $userId,movie:{ movieId: $movieId,title: $title,genres: $genres},rating: $rating, timestamp: $timestamp }) RETURN m.userId AS userId;",
-        userId=365,
-        movieId=7526,
-        title="Lord of the Rings: The Fellowship of the Ring, The (2001)",
-        genres=["Adventure", "Fantasy"],
-        rating=5,
-        timestamp=45554125,
-        database_="memgraph",
-    )
+#     records, summary, keys = client.execute_query(
+#         "MERGE (m:Movie {userId: $userId,movie:{ movieId: $movieId,title: $title,genres: $genres},rating: $rating, timestamp: $timestamp }) RETURN m.userId AS userId;",
+#         userId=365,
+#         movieId=7526,
+#         title="Lord of the Rings: The Fellowship of the Ring, The (2001)",
+#         genres=["Adventure", "Fantasy"],
+#         rating=5,
+#         timestamp=45554125,
+#         database_="memgraph",
+#     )
 
-    # Get the result
-    for record in records:
-        print(record["userId"])
+#     # Get the result
+#     for record in records:
+#         print(record["userId"])
